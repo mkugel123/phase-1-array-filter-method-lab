85cd2e5bb9f2e10c9956df8359b6cd51c4e785d1
Zalman <moishkugel@gmail.com>
2022-08-21 12:15:46 -0400

    commit tree 4f1411cebdd7fd8890d34d1db8bd06f8bbac8147

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (matchingName) {
+        return matchingName.toUpperCase() === name.toUpperCase();
+    });
+
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(function (name) {
+        return name.startsWith(letter)
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (matchingName) {
+        return matchingName.name === name;
+     })
+}
